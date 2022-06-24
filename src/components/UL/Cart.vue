e8d1633665598ff494d6f6dd9ef4d723694217e3
marat <mrxpro2003@gmail.com>
2022-06-24 12:56:43 +0500

    узгаришлар

---
+++ src/components/UL/Cart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card">
+        <span class="card-name">{{name}}</span>
+        <span class="card-title">{{title}}</span>
+        <div class="card-bady">
+           <slot>{{item.descr}}</slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        name:{
+            type:String,
+            required:true,
+        },
+
+        title:{
+            type:String,
+            required:true,
+        },
+
+        descr:{
+            type:String,
+            required:true,
+        },
+    }
+}
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,40 @@
 <template>
-    <h1>Hello Home</h1>
+    <div class="mb-body">
+        <h1>Hello Home</h1>
+        
+        <div class="card__wrapper" v-for="item in items" :key="item.id">
+            <Cart :name="`${item.lvl}`" :title="item.title">
+                {{item.descr}}
+            </Cart>
+        </div>
+    </div>
 </template>
+
+<script>
+import Cart from '@/components/UL/Cart.vue';
+export default {
+    components:{
+        Cart
+    },
+
+    data(){
+        return{
+            items:[
+                {
+                    id: 1,
+                    title:'Archer',
+                    descr:'Archer ipsum dolor, sit amet consectetur adipisicing elit. Modi, aut!',
+                    lvl:4
+                },
+
+                {
+                    id: 2,
+                    title:'Wizard',
+                    descr:'Wizard ipsum dolor, sit amet consectetur adipisicing elit. Modi, aut!',
+                    lvl:6
+                },
+            ]
+        }
+    }
+}
+</script>
